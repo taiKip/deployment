dd34a3eb1cc63611291f45df478063f802fab9d5
tarus <victortarus6@gmail.com>
2024-05-12 21:37:16 +0300

    created user entity

--- src/main/java/com/example/githubactions/user/User.java
+++ src/main/java/com/example/githubactions/user/User.java
@@ -1,4 +1,16 @@
 package com.example.githubactions.user;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String username;
+
 }
